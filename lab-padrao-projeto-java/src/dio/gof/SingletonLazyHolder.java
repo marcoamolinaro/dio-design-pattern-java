9391ace128f188ea203e4326b803a79206c7c2b1
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-11-09 10:07:55 -0300

    Add Singleton implementation

---
+++ lab-padrao-projeto-java/src/dio/gof/SingletonLazyHolder.java
@@ -0,0 +1,22 @@
+package dio.gof;
+
+/**
+ * Singleton "Lazy Holder"
+ *
+ * @see <a href="https://stackoverflow.com/a/24018148>Referência</a>"
+ *
+ */
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder {
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia() {
+        return InstanceHolder.instancia;
+    }
+}
